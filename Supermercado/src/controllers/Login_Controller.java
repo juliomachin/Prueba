1fe04dacf5d9299a9f4cd80c3683d012e192a872
jmachr00 <jmachr00@estudiantes.unileon.es>
2018-12-05 00:55:52 +0100

    First Commit

---
+++ Supermercado/src/controllers/Login_Controller.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
+
+public class Login_Controller {
+
+
+	@FXML
+	private JFXTextField user;
+
+	@FXML
+	private JFXPasswordField pass;
+
+	@FXML
+	private JFXButton log;
+
+	@FXML
+	void initialize() {
+		
+	}
+}
